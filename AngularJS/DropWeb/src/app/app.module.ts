cfbf5d2cccebd48f3761c5bb12872592a7729d23
Unknown <bnshanahan@gmail.com>
2018-02-23 11:23:35 +0000

    qr generator working

--- AngularJS/DropWeb/src/app/app.module.ts
+++ AngularJS/DropWeb/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { CenterComponentComponent } from './center-component/center-component.component';
+import { BackgroundComponentComponent } from './background-component/background-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    CenterComponentComponent,
+    BackgroundComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxQRCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
